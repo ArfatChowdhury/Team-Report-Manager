6fce8bf2e8a161b981d5965ae4aec0c8fcdb48b9
ArfatChowdhury <138390501+ArfatChowdhury@users.noreply.github.com>
2026-04-30 00:32:44 +0600

    fix login issue

---
+++ src/api/usersApi.ts
@@ -0,0 +1,16 @@
+import client from './client';
+
+export const getAllUsers = async () => {
+  const response = await client.get('/users');
+  return response.data;
+};
+
+export const createUser = async (userData: any) => {
+  const response = await client.post('/users', userData);
+  return response.data;
+};
+
+export const deleteUser = async (userId: string) => {
+  const response = await client.delete(`/users/${userId}`);
+  return response.data;
+};
